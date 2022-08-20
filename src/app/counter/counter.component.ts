465ea02fb007018e70724f8f42a2a6b312d0ff5e
Ebo Arcadia <yiboli.coding@gmail.com>
2022-08-20 20:17:13 -0300

    implement ng on changes, ng do check lifecycle hooks methods

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChange, SimpleChanges, OnChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnChanges, OnInit, DoCheck {
   name: string = "";
-  @Input() counter = 0;
+  @Input() counter = 1;
   @Output() counterChange = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("------ngOnInit is called-----!!!")
+  }
+
+  ngOnChanges(valueChanges: SimpleChanges) {
+    console.info("-----ng on change is called----!!!")
+    console.log(valueChanges['prop'])
+  }
+
+  ngDoCheck() {
+    console.log("-----ng Do Check is incoked----!!!")
   }
 
   increment() {
