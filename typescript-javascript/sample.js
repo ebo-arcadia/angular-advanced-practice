a479b5eb6b90d9b7787dbf8859f0d6380828f342
Ebo Arcadia <yiboli.coding@gmail.com>
2022-08-25 21:56:43 -0400

    implement typescripte object literal

--- typescript-javascript/sample.js
+++ typescript-javascript/sample.js
@@ -3,8 +3,31 @@ var content = "I can only be string and nothing else!";
 var blackOrWhite = false;
 var objectLiterals = ["faith", "hope", "love"];
 var stringNum = '888';
+var country = {
+    countryName: "Bermuda",
+    population: 69000,
+    armyNum: 2000,
+    manageArmy: function () {
+        if (this.armyNum >= 1500) {
+            return "time to reduce army force!";
+        }
+        else {
+            return "we need more army soldiers!";
+        }
+    },
+    makePolicy: function () {
+        return "passing a new tax law";
+    }
+};
+country.constitution = "british parliment";
 console.log(ticketNumber);
 console.log(content);
 console.log(blackOrWhite);
 console.log(objectLiterals);
 console.log(stringNum);
+console.log(country.countryName);
+console.log(country.population);
+console.log(country.armyNum);
+console.log(country.manageArmy());
+console.log(country.makePolicy());
+console.log(country.constitution);
--- typescript-javascript/sample.ts
+++ typescript-javascript/sample.ts
@@ -3,9 +3,29 @@ var content: string =  "I can only be string and nothing else!"
 var blackOrWhite: boolean = false
 var objectLiterals: any = ["faith", "hope", "love"]
 var stringNum: string = '888'
+var country: any = {
+    countryName: "Bermuda",
+    population: 69_000,
+    armyNum: 2000,
+    manageArmy: function() {
+        if (this.armyNum >= 1500) { return "time to reduce army force!"}
+        else { return "we need more army soldiers!"}
+    },
+    makePolicy: function() {
+        return "passing a new tax law"
+    }
+}
+
+country.constitution = "british parliment"
 
 console.log(ticketNumber)
 console.log(content)
 console.log(blackOrWhite)
 console.log(objectLiterals)
 console.log(stringNum)
+console.log(country.countryName)
+console.log(country.population)
+console.log(country.armyNum)
+console.log(country.manageArmy())
+console.log(country.makePolicy())
+console.log(country.constitution)
