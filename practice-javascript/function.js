1316b31f946b2f50ca081376cd2899eafe652f4f
Ebo Arcadia <yiboli.coding@gmail.com>
2022-08-31 19:21:26 -0400

    brush up function declaration, operation basics

---
+++ practice-javascript/function.js
@@ -0,0 +1,12 @@
+let simpleMath = function(x, y) {
+    let result = (x * y) / 100;
+    return result
+};
+
+console.log(simpleMath())
+let result_1 = simpleMath(2000, 5123)
+console.info(result_1)
+let result_2 = simpleMath(10, 5)
+console.info(result_2)
+let result_3 = result_1 * result_2
+console.log(result_3)
