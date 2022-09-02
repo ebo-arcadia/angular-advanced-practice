e195d95cfd91127fa2aacff94aa5db0f09a445b2
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-02 09:14:25 -0400

    practice default arguments when working with function, scopes of access global, local, block level of variables when working with function

--- practice-javascript/function.js
+++ practice-javascript/function.js
@@ -40,3 +40,58 @@ let calculateEquity = function(downPayment) { return getDownPayment(downPayment)
 
 let closingDeal = calculateEquity(5_000)
 console.log(closingDeal);
+
+// default arguments in a function
+let motion = function(velocity=1000, accelerate=54, limit=888) {
+    let position = velocity * (accelerate / limit)
+    let motion = position * 3.14
+    return motion
+}
+
+let motion_1 = motion(120, 4, 20)
+console.log("values supplied to all arguments: ", motion_1)
+
+let motion_2 = motion()
+console.log("values not supplied at all: ", motion_2)
+
+// scope of variables
+
+// global variables
+var global_var_1 = "earth planet";
+let global_var_2 = "galaxy";
+console.log("print global variable 1: ", global_var_1)
+console.log("print global variable 2: ", global_var_2)
+
+let nested_function = function( parameter ) {
+    var local_var_1 = "country";
+    let local_var_2 = "state";
+    console.log("local variable 1: ", local_var_1);
+    console.log("local variable 2: ", local_var_2);
+    console.log("call global variable 1 from inside this function: ", global_var_1);
+    console.log("call global variable 2 from inside this function: ", global_var_2);
+
+    if (local_var_1 == "country") {
+        var block_var_1 = "county";
+        let block_var_2 = "street";
+        console.log("access local variable 1 from inside 1st if block: ", local_var_1);
+        console.log("access local variable 2 from inside 1st if block: ", local_var_2);
+        console.log("access block variable 1 from inside 1st if block: ", block_var_1);
+        console.log("access block variable 2 from inside 1st if block: ", block_var_2);
+    }
+    if (parameter) {
+        var block_var_3 = "community";
+        var block_var_4 = "building";
+        console.log("access local variable 1 from inside 2nd if block: ", local_var_1);
+        console.log("access local variable 2 from inside 2nd if block: ", local_var_2);
+        console.log("access block variable 1 from inside 2nd if block: ", block_var_3);
+        console.log("access block variable 2 from inside 2nd if block: ", block_var_4);
+    }
+
+    console.log("access block variable 1 from the local scope: ", block_var_1);
+    console.log("access block variable 3 from the local scope: ", block_var_3);
+}
+
+// this won't work
+// console.log("access local variable 1 outside the local function scope: ", local_var_1)
+
+nested_function("true")
