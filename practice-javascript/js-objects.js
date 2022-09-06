c416d26c96ce83e96cc6d9361ce3016c8a90c0a9
Ebo Arcadia <yiboli.coding@gmail.com>
2022-09-06 11:06:24 -0400

    create real world scenarios using concept of object, properties, methods

--- practice-javascript/js-objects.js
+++ practice-javascript/js-objects.js
@@ -34,3 +34,53 @@ console.info(city.retrieveAttraction(city.attractions))
 // use reference to the object outside the object
 this.globalThisAttr = "global variable value"
 console.log("what keyword this is referred to outside the object?: ", this.globalThisAttr);
+
+// real world scenarios
+// object: developer
+
+var developer = {
+    firstName: "Jake",
+    lastName: "Wesker",
+    title: "Mid-level Application developer",
+    salary: 75_000,
+    workExperienceInYears: 4,
+    isEligibleForPromotion: false,
+    getFullName: function() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    promote: function() {
+        if (this.title == "Application developer") {
+            if (this.workExperienceInYears >= 1 && this.workExperienceInYears <= 3) {
+                this.isEligibleForPromotion = true;
+                this.title = "Mid-level Application developer";
+                this.salary += this.salary * 0.10;
+            }
+            else {
+                this.isEligibleForPromotion = false;
+            }
+        }
+
+        else if (this.title == "Mid-level Application developer") {
+            if (this.workExperienceInYears > 3 && this.workExperienceInYears <= 5) {
+                this.isEligibleForPromotion = true;
+                this.title = "Architect consultant";
+                this.salary += this.salary * 0.30;
+            }
+            else {
+                this.isEligibleForPromotion = false;
+            }
+        }
+        return this.isEligibleForPromotion;
+    }
+
+};
+
+
+if (developer.promote() == true) {
+    console.log("Dear " + developer.getFullName() + ", Congratulations! You are promoted!")
+    console.log("Your new title is: " + developer.title);
+    console.info("Your new salary is: " + developer.salary)
+}
+else {
+    console.info("You will be promoted in the near future!")
+};
